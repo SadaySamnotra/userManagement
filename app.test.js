0ee450e88936f7d93dcb641bed312aec373c8bb3
SadaySamnotra <sadaysamnotra@gmail.com>
2024-10-16 12:21:36 +0530

    new change

--- app.test.js
+++ app.test.js
@@ -27,12 +27,14 @@ describe('Testing API Endpoints',()=>{
         expect(res.statusCode).toBe(200);
         expect(res.body.students).toEqual(expectedResponses.allStudents);
     });
+
     test('/Post request to post data on the server',async()=>{
         mock.create.mockResolvedValue({id:1,...testData.newStudent});
         const response = await request(app).post('/student').send(testData.newStudent);
         expect(response.statusCode).toBe(201);
         expect(response.body.student).toEqual(expectedResponses.postStudent);
     });
+
     test('PUT /student/:id - should update student details', async () => {
         mock.update.mockResolvedValue([1]); 
         mock.findByPk.mockResolvedValue({ ...testData.updateDetails, id: studentID }); 
@@ -42,10 +44,12 @@ describe('Testing API Endpoints',()=>{
         expect(response.body).toHaveProperty('student');
         expect(response.body.student.firstName).toBe(expectedResponses.updatedStudent.student.firstName); 
     });
+
     test('DELETE /student/:id - should delete a student', async () => {
         mock.destroy.mockResolvedValue([1]);
         const response = await request(app).delete(`/student/${studentID}`);
         expect(response.statusCode).toBe(204);
         expect(response.body).toEqual({});
     });
+    
 });
